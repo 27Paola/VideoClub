1fce0891c894fc67bf453c72cff25f69ced24e1d
Paola Sanchez <yineth2paola2@gmail.com>
2023-02-08 23:37:31 -0500

    Frontend

--- conection.js
+++ conection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://<username>:<password>@cluster0.h0tfu5q.mongodb.net/VideoClubCRUD')
+mongoose.connect('mongodb+srv://PaolaSanchez:paola123@cluster0.h0tfu5q.mongodb.net/VideoClubCRUD')
 
 const objectbd = mongoose.connection
 
